c6e80a12ec02e781d7b1bf5ca07b8ad39c0fc880
maica888 <ieph.maica888@gmail.com>
2024-07-23 13:33:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'PHAT UGLY',
-    introDesc: `CAN YOU BUY ME FOOD NOW!!!😊😚`,
+    introDesc: `CAN YOU BUY ME FOOD NOW!!!`,
     btnIntro: '^^WHY???^^',
-    title: 'I WANT TO EATTT! I'M HUNGRY!😢',
+    title: 'I WANT TO EATTT! I'M HUNGRY!',
     desc: 'BUY FOOD NOW!',
     btnYes: 'YES!!😍',
-    btnNo: 'NO 💔:3',
+    btnNo: 'NO :3',
     question: 'YOU WILL BUY ME FOOD NOW?😍',
     btnReply: 'YEHEYYY!',
     reply: 'YES! BECAUSE YOU'RE SO PRETTY!❤️',
     mess: 'YEHEY! THANK YOU!😘😘',
     messDesc: 'Order now.',
-    btnAccept: 'Okay Mama❤️',
+    btnAccept: 'Okay Mama',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
